b26bfaaf2eb86445771256a3a8c5a16f7181a9a7
L1edge <koroliuk.artur@student.uzhnu.edu.ua>
2025-09-30 09:01:52 +0300

    Lab 1 export constants

---
+++ constants/elements-lab1.tsx
@@ -0,0 +1,10 @@
+export const intValue: number = 12;
+export const intValue2: number = -100;
+export const intValue3: number = 0x80;
+export const intValue4: number = -32768;
+export const intValue5: bigint = 9223372036854775807n;
+export const floatValue: GLfloat = 10235.34;
+export const stringValue: string = "a";
+export const stringValue2: string = "Hello World";
+export const truthValue: boolean = true;
+export const stringValue3: string = "twelve"
